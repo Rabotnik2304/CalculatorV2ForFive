4b3e95850a3c556a6e8138cc4e495353e1c6ce49
informant <artemiy.solodkin@mail.ru>
2022-12-03 23:29:27 +0500

    Пункт 2 в процессе

--- CalculatorV2ForFive/Program.cs
+++ CalculatorV2ForFive/Program.cs
@@ -14,7 +14,7 @@ namespace CalculatorV2ForFive
                 Console.WriteLine("Примечание: Данная программа не поддерживает операции с числами большими по модулю 2 147 483 647");
 
                 Console.WriteLine();
-                Console.WriteLine("Нажмите 1 чтобы перевести целое число меньшее 256 в дополнительный код");
+                Console.WriteLine("Нажмите 1 чтобы перевести целое число по модулю меньшее 256 в дополнительный код");
                 Console.WriteLine("Нажмите 2 чтобы сложить целые числа(положительные и отрицательные) с использованием дополнительного кода");
                 Console.WriteLine("Нажмите 3 чтобы перевести любое вещественное число в формат с плавающей точкой");
                 Console.WriteLine("Нажмите 4 чтобы проссумировать два вещественных числа в формате с плавающей точкой");
@@ -31,7 +31,7 @@ namespace CalculatorV2ForFive
                     }
                     else if (operation == "2")
                     {
-                        Console.WriteLine("Мы не можем сделать эту операцию");
+                        AdditionalSumStart();
                     }
                     else if (operation == "3")
                     {
@@ -43,15 +43,19 @@ namespace CalculatorV2ForFive
                     }
                     else
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine();
                         Console.WriteLine("Мы не можем сделать эту операцию");
+                        Console.ResetColor();
                     }
                 }
                 catch (ArgumentException ex)
                 {
                     Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("Ошибка:");
                     Console.WriteLine(ex.Message);
+                    Console.ResetColor();
                 }
 
                 Console.WriteLine();
@@ -67,9 +71,56 @@ namespace CalculatorV2ForFive
             }
         }
 
+        private static void AdditionalSumStart()
+        {
+            Console.WriteLine("Введите через пробел два целых числа, которые вы хотите сложить");
+            Console.WriteLine("(Внимание, в случае если сумма чисел больше 127 калькулятор будет выводить неверные ответы)");
+            string sumReadLine = Console.ReadLine().Trim();
+            string[] sumSplit = sumReadLine.Split(" ");
+
+            if (sumSplit.Length < 2)
+            {
+                throw new ArgumentException("Вы не ввели все числа");
+            }
+            if (sumSplit.Length > 2)
+            {
+                throw new ArgumentException("Вы ввели лишние числа");
+            }
+
+            if (int.TryParse(sumSplit[0], out int number1))
+                number1 = number1;
+            else
+                throw new ArgumentException("Ваше первое число некоректно");
+
+            if (int.TryParse(sumSplit[1], out int number2))
+                number2 = number2;
+            else
+                throw new ArgumentException("Ваше второе число некоректно");
+
+            Console.WriteLine();
+            Console.WriteLine("Перед тем как производить сложение, нужно перевести оба числа в дополнительный код:");
+            
+            string additionalNumber1 = ConverterToAdditional(number1);
+            string additionalNumber2 = ConverterToAdditional(number2);
+            Console.WriteLine();
+            Console.WriteLine("Теперь производим суммирование двух чисел в дополнительном коде:");
+            Console.WriteLine();
+            string sum = CorrectSum(additionalNumber1, additionalNumber2);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Итоговая сумма в дополнительном коде: {0}", sum);
+            Console.ResetColor();
+
+            if (sum.Substring(0, 1) == "1")
+            {
+                
+            }
+
+        }
+
         private static void ConverterToAdditionalStart()
         {
-            Console.WriteLine("Введите целое число, которое вы хотите перевести в дополнительный код");            
+            Console.WriteLine("Введите целое число, которое вы хотите перевести в дополнительный код");
 
             string readLine = Console.ReadLine().Trim();
 
@@ -77,9 +128,21 @@ namespace CalculatorV2ForFive
                 number1 = number1;
             else
                 throw new ArgumentException("Ваше число некорректно");
+            
+            ConverterToAdditional(number1); 
+        }
+
+        private static string ConverterToAdditional(int number1)
+        {
+            int startNumber1 = number1;
             string binNumber;
             Console.WriteLine();
-            if (number1>=0)
+            if (Math.Abs(number1) >= 256)
+            {
+                throw new ArgumentException("Ваше число по модулю больше либо равно 256");
+            }
+
+            if (number1 >= 0)
             {
                 Console.WriteLine("Т.к. число {0} - не отрицательное, то, чтобы найти его представление в дополнительном коде нам нужно:", number1);
                 Console.WriteLine("1. перевести это число в двоичную систему счисления ");
@@ -89,8 +152,6 @@ namespace CalculatorV2ForFive
                 Console.WriteLine();
                 Console.WriteLine("Дописываем к числу {0} нули слева, пока оно не станет длины 8", binNumber);
                 binNumber = binNumber.PadLeft(8, '0');
-                Console.WriteLine("Итоговый ответ: {0}", binNumber);
-                
             }
             else
             {
@@ -105,14 +166,14 @@ namespace CalculatorV2ForFive
                 binNumber = RightFromDecToBinary(number1);
                 Console.WriteLine();
                 Console.WriteLine("Дописываем к числу {0} нули слева, пока оно не станет длины 8", binNumber);
-                binNumber=binNumber.PadLeft(8, '0');
+                binNumber = binNumber.PadLeft(8, '0');
                 Console.WriteLine("Итоговое число: {0}", binNumber);
                 Console.WriteLine();
-                
+
                 Console.WriteLine("Теперь заменим в числе {0} все 0 на 1 и все 1 на 0", binNumber);
-                
+
                 StringBuilder reverseBinNumber = new StringBuilder();
-                foreach(char c in binNumber)
+                foreach (char c in binNumber)
                 {
                     if (c == '0')
                     {
@@ -128,10 +189,16 @@ namespace CalculatorV2ForFive
                 Console.WriteLine();
                 Console.WriteLine("Теперь добавляем к числу {0} 1 и получаем итоговый ответ", reverseBinNumber);
                 Console.WriteLine();
-                CorrectSum(reverseBinNumber.ToString(),"1");
-
+                binNumber = CorrectSum(reverseBinNumber.ToString(), "1");
+  
             }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Представление числа {0} в дополнительном коде - {1}", startNumber1, binNumber);
+            Console.ResetColor();
+            return binNumber;
         }
+
         static int FromAnyToDec(string number, int baze)
         {
 
@@ -223,13 +290,13 @@ namespace CalculatorV2ForFive
                     sum.Append(res - base1);
                     if (i == len - 1)
                     {
-                        Console.WriteLine("Т.к. у нас получилось число {0} и это последнее число в сложениии то мы пишем число {1} и дописываем слева 1", base1, res - base1);
+                        Console.WriteLine("Т.к. у нас получилось число большее {0} и это последнее число в сложениии то мы пишем число {1} и дописываем слева 1", base1-1 , res - base1);
                         sum.Append("1");
 
                     }
                     else
                     {
-                        Console.WriteLine("Т.к. у нас получилось число {0}, то мы пишем число {1} и добаляем 1 к следующему разряду", base1, res - base1);
+                        Console.WriteLine("Т.к. у нас получилось число большее {0}, то мы пишем число {1} и добаляем 1 к следующему разряду", base1-1, res - base1);
                         des = 1;
                     }
                 }
@@ -246,8 +313,6 @@ namespace CalculatorV2ForFive
             string res1 = sum.ToString();
             res1 = new string(res1.Reverse().ToArray());
             int lenRes = res1.Length;
-
-
 
             Console.Write(" ");
             Console.WriteLine(maxNum.PadLeft(lenRes));
@@ -269,7 +334,7 @@ namespace CalculatorV2ForFive
         static string RightFromDecToBinary(int number)
         {
             int baze = 2;
-            
+            int numberStart = number;
             StringBuilder builder = new StringBuilder();
 
             Console.WriteLine();
@@ -295,7 +360,7 @@ namespace CalculatorV2ForFive
             Console.WriteLine();
             Console.WriteLine("Записываем получившиеся остатки от деления задом на перёд");
             string result = string.Join("", builder.ToString().Reverse());
-            Console.WriteLine("Итоговый ответ: {0}", result);
+            Console.WriteLine("Число {0} в двоичной системе счисления имеет вид: {1}", numberStart, result);
 
             return result;
         }
